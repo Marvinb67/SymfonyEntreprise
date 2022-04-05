7dc060bb574514fc5511ec27485b66f421b3b523
Marvinb67 <brassler.marvin@gmail.com>
2022-04-05 11:07:26 +0200

    Exercice d'introduction à symfony

---
+++ migrations/Version20220405082925.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220405082925 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE employe (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(20) NOT NULL, date_naissance DATE NOT NULL, adresse VARCHAR(50) DEFAULT NULL, cp VARCHAR(10) DEFAULT NULL, ville VARCHAR(30) DEFAULT NULL, date_embauche DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE entreprise (id INT AUTO_INCREMENT NOT NULL, raison_sociale VARCHAR(50) NOT NULL, siret VARCHAR(20) NOT NULL, adresse VARCHAR(50) DEFAULT NULL, cp VARCHAR(10) DEFAULT NULL, ville VARCHAR(30) DEFAULT NULL, date_creation DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE employe');
+        $this->addSql('DROP TABLE entreprise');
+    }
+}
